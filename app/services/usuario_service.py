7ccae8ec4dd6c81ec45d3fc9a00e8c81c2c8c672
JSantiE <josh1621@hotmail.com>
2026-09-02 19:01:07 -0500

    agregar funciones de login

---
+++ app/services/usuario_service.py
@@ -0,0 +1,37 @@
+from app.database.db import get_connection
+from app.models.usuario import Usuario
+
+class UsuarioService():
+    
+    @classmethod
+    def login(self, user):
+        try:
+            connection = get_connection()
+            with connection.cursor() as cursor:
+                query = "SELECT idUsuario, usuario, contraseña FROM usuario WHERE usuario = '{}'".format(user.username)
+                cursor.execute(query)
+                row = cursor.fetchone()
+                
+                if row != None:
+                    user = Usuario(row[0], row[1], Usuario.check_password(row[2], user.password))
+                    return user
+                else:
+                    return None
+        except Exception as ex:
+            raise Exception(f"Error al autenticar el usuario: {ex}")
+
+    @classmethod
+    def get_user_by_id(self, user_id):
+        try:
+            connection = get_connection()
+            with connection.cursor() as cursor:
+                query = "SELECT idUsuario, usuario FROM usuario WHERE idUsuario = '{}'".format(user_id)
+                cursor.execute(query)
+                row = cursor.fetchone()
+                
+                if row != None:
+                    return Usuario(row[0], row[1], None)
+                else:
+                    return None
+        except Exception as ex:
+            raise Exception(f"Error al obtener el usuario por ID: {ex}")
