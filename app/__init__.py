091649c1853a0907b4a4b11c434969f2545cfa87
JSantiE <josh1621@hotmail.com>
2026-09-02 19:01:39 -0500

    inicializar loginmanager

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,21 @@
 from flask import Flask
+from config import Config
+from flask_login import LoginManager
+
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
     
+    login_manager.init_app(app)
+    
+    #Cargar configuración
+    app.config.from_object(Config)
+    
+    #Registrar rutas
     from app.routes.main import main
+    
     app.register_blueprint(main)
+    
     
     return app
